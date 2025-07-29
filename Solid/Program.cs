c0eed512100714cda0cc0deccb01a89b5203401a
sothini <kaongallostam@gmail.com>
2025-07-29 21:58:05 +0200

    remove nested classes

--- Solid/Program.cs
+++ Solid/Program.cs
@@ -6,54 +6,53 @@ using System.Threading.Tasks;
 
 namespace Solid
 {
+    //*
+    //The below simple example demostrates the interface segregation principle 
+    //Where a class is not forced to implement methods that it does not need 
+    //
+    //*//
+    public interface IArea
+    {
+        decimal getArea();
+    }
+
+    public interface IPerimeter
+    {
+        decimal getPerimiter();
+    }
+
+    public class Rectangle : IArea, IPerimeter
+    {
+        public decimal getArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal getPerimiter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ICircumference
+    {
+        decimal getCircumference();
+    }
+
+    public class Circle : IArea, ICircumference
+    {
+        public decimal getArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal getCircumference()
+        {
+            throw new NotImplementedException();
+        }
+    }
     internal class Program
     {
-
-        //*
-        //The below simple example demostrates the interface segregation principle 
-        //Where a class is not forced to implement methods that it does not need 
-        //
-        //*//
-        public interface IArea
-        {
-            decimal getArea();
-        }
-
-        public interface IPerimeter 
-        {
-            decimal getPerimiter();
-        }
-
-        public class Rectangle : IArea , IPerimeter
-        {
-            public decimal getArea()
-            {
-                throw new NotImplementedException();
-            }
-
-            public decimal getPerimiter()
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public interface ICircumference 
-        { 
-            decimal getCircumference();
-        }
-
-        public class Circle : IArea,  ICircumference
-        {
-            public decimal getArea()
-            {
-                throw new NotImplementedException();
-            }
-
-            public decimal getCircumference()
-            {
-                throw new NotImplementedException();
-            }
-        }
         static void Main(string[] args)
         {
             var rectangle = new Rectangle();
